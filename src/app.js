5c975e68a8ec09e02267d543a06214a70c8662d1
tashinuke <stasonukesweet@gmail.com>
2020-09-05 13:42:56 +0300

    add func

--- src/app.js
+++ src/app.js
@@ -3,6 +3,22 @@ import home from "./routes/home";
 import article from "./routes/article";
 import compression from "compression";
 
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
 var port = normalizePort(process.env.PORT || '3000');
 const app = express();
 
